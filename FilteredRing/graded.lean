1f251ddc9395123c905e5d0081afda3d951fe0c6
yuanyi-350 <kysyy1@126.com>
2024-10-05 15:41:33 +0800

    Update graded.lean

--- FilteredRing/graded.lean
+++ FilteredRing/graded.lean
@@ -7,7 +7,9 @@ variable {ι : Type v} [OrderedAddCommMonoid ι] [DecidableEq ι] [PredOrder ι]
 
 variable (F : ι → AddSubgroup R) [FilteredRing F]
 
+instance {i : ι}: F (PredOrder.pred i) ≤ F i :=
+  FilteredRing.mono (PredOrder.pred i) (PredOrder.pred_le i)
 
-def gradedMap (F : ι → AddSubgroup R) [FilteredRing F] : ι → AddSubgroup R := sorry
+def gradedMap {i : ι} : Module R ((F i) ⧸ (F (PredOrder.pred i)).addSubgroupOf (F i)) := sorry
 
-instance : GradedRing (gradedMap F) := sorry
+-- instance : GradedRing (gradedMap F) := sorry
