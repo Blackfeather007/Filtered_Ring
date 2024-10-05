b73d63ef9a908643647243e503d776d5708afe85
Blackfeather007 <2100017455@stu.pku.edu.cn>
2024-10-05 18:54:02 +0800

    Update graded.lean

--- FilteredRing/graded.lean
+++ FilteredRing/graded.lean
@@ -14,24 +14,16 @@ abbrev GradedPiece (i : ι) : Type u := (F i) ⧸ (F (Order.pred i)).addSubgroup
 
 def gradedMul {i j : ι} : GradedPiece F i → GradedPiece F j → GradedPiece F (i + j) := by
   intro x y
-
-  -- set x' := x.out' with hx
-  -- set y' := y.out' with hy
-  -- have hx' : (x' : R) ∈ (F i : Set R) := by exact Subtype.coe_prop x'
-  -- have hy' : (y' : R) ∈ (F j : Set R) := by exact Subtype.coe_prop y'
-  -- have t1 : (F i : Set R) * F j ≤ F (i + j) := FilteredRing.mul_mem i j
-  -- have t2 : (x' : R) * (y' : R) ∈ (F i : Set R) * F j := by
-  --   exact Set.mul_mem_mul hx' hy'
-  -- have : (x' : R) * (y' : R) ∈ F (i + j) := by exact t1 t2
   refine Quotient.map₂' (fun x y ↦ ⟨x.1 * y.1, Filtration.mul_mem F i j (Set.mul_mem_mul x.2 y.2)⟩)
     ?_ x y
   intro x₁ x₂ hx y₁ y₂ hy
   simp [QuotientAddGroup.leftRel_apply, AddSubgroup.mem_addSubgroupOf] at hx hy ⊢
-  #check Set.mul_mem_mul
-  -- have hh : -↑x₁ + ↑x₂ + (x₁ + ↑x₂) ∈ F (Order.pred (i + j)) := by sorry
+  have eq : - (x₁.1 * y₁) + x₂ * y₂ = (- x₁ + x₂) * y₁ + x₂ * (- y₁ + y₂) := by noncomm_ring
+  rw [eq]
+  obtain h1 := Filtration.mul_mem F (Order.pred i) j (Set.mul_mem_mul hx y₁.2)
+  obtain h2 := Filtration.mul_mem F i (Order.pred j) (Set.mul_mem_mul x₂.2 hy)
+  have : Order.pred i + j ≤ Order.pred (i + j) := sorry
   sorry
-
-
 
 
   -- have : x.out'.1 ∈ F i := x.out'.2
@@ -39,12 +31,26 @@ def gradedMul {i j : ι} : GradedPiece F i → GradedPiece F j → GradedPiece F
   -- let z : F (i + j) := ⟨_, hh⟩
   -- exact @QuotientAddGroup.mk' (F (i + j)) _ ((F (Order.pred (i + j))).addSubgroupOf (F (i + j))) _ z
 
+instance (F : ι → AddSubgroup R) [FilteredRing F] : DirectSum.GSemiring (GradedPiece F) where
+  mul := gradedMul F
+  mul_zero a := by
 
-instance {i j : ι} : HMul (GradedPiece F i) (GradedPiece F j) (GradedPiece F (i + j)) where
-  hMul := gradedMul F
+    sorry
+  zero_mul := sorry
+  mul_add := sorry
+  add_mul := sorry
+  one := sorry
+  one_mul := sorry
+  mul_one := sorry
+  mul_assoc := sorry
+  gnpow := sorry
+  gnpow_zero' := sorry
+  gnpow_succ' := sorry
+  natCast := sorry
+  natCast_zero := sorry
+  natCast_succ := sorry
 
 
--- def inducedGradedRing : Type w := Σ i, (F i) ⧸ (F (pred i)).addSubgroupOf (F i)
 -- open AddSubgroup PredOrder
 
 -- variable {R : Type u} [Ring R]
