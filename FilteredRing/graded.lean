1b1ca520ffb11bd4ac24098473657af38e7f5374
yuanyi-350 <kysyy1@126.com>
2024-10-05 16:50:01 +0800

    Update graded.lean

--- FilteredRing/graded.lean
+++ FilteredRing/graded.lean
@@ -14,4 +14,16 @@ instance {i : ι}: F (PredOrder.pred i) ≤ F i :=
 
 def gradedMap {i : ι} := ((F i) ⧸ (F (PredOrder.pred i)).addSubgroupOf (F i))
 
--- instance : GradedRing (gradedMap F) := sorry
+instance : GradedRing (F) where--It may be not correct,attention!
+  one_mem := FilteredRing.one
+  mul_mem := by
+    intro i j x y hx hy
+    have t1 : (F i : Set R) * F j ≤ F (i + j) := FilteredRing.mul_mem i j
+    have t2 : x * y ∈ (F i : Set R) * F j := Set.mul_mem_mul hx hy
+    exact t1 t2
+  decompose' := by
+    intro r
+
+    sorry
+  left_inv := sorry
+  right_inv := sorry
