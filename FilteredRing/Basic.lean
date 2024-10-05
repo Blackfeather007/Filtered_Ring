32c09e9cc51d28012682ff17e66d64bd445f3573
Blackfeather007 <2100017455@stu.pku.edu.cn>
2024-10-05 13:39:41 +0800

    Update Basic.lean

--- FilteredRing/Basic.lean
+++ FilteredRing/Basic.lean
@@ -4,18 +4,16 @@ universe u v
 
 open Pointwise
 
-variable (R : Type u) (ι : Type v) [Ring R] [OrderedAddCommMonoid ι] [DecidableEq ι]
+variable {R : Type u} {ι : Type v} [Ring R] [OrderedAddCommMonoid ι] [DecidableEq ι]
 
-class FilteredRing where
-  fil : ι → AddSubgroup R
-  mono : ∀ i ≤ j, fil i ≤ fil j
-  one : 1 ∈ fil 0
-  mul_mem : ∀ i j : ι, (fil i : Set R) * fil j ≤ fil (i + j)
+class FilteredRing (F : ι → AddSubgroup R)where
+  mono : ∀ i ≤ j, F i ≤ F j
+  one : 1 ∈ F 0
+  mul_mem : ∀ i j : ι, (F i : Set R) * F j ≤ F (i + j)
 
-variable [F : FilteredRing R ι]
+variable (F : ι → AddSubgroup R)
 variable (M : Type u) [AddCommGroup M] [Module R M]
 
-class FilteredModule where
-  fil : ι → Submodule R M
-  mono : ∀ i ≤ j, fil i ≤ fil j
-  smul_mem : ∀ i j, (F.fil i : Set R) • fil j ≤ fil (i + j)
+class FilteredModule (F' : ι → Submodule R M) where
+  mono : ∀ i ≤ j, F' i ≤ F' j
+  smul_mem : ∀ i j, (F i : Set R) • F' j ≤ F' (i + j)
