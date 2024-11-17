99bd8b4443149306042596e9b2bee09faf3efa2b
AlbertJ-314 <albertjiao314@gmail.com>
2024-11-17 14:17:07 +0800

    Update tensor.lean

--- FilteredRing/tensor.lean
+++ FilteredRing/tensor.lean
@@ -2,9 +2,31 @@ import Mathlib
 import FilteredRing.Basic
 import FilteredRing.filtered_category
 
-variable {R : Type u} {ι : Type v} [Ring R] [OrderedAddCommMonoid ι] {σ : Type o} [SetLike σ R]
-  (F : ι → σ) [FilteredRing F]
+variable {R : Type u} {ι : Type v} [CommSemiring R] [OrderedAddCommMonoid ι] [DecidableEq ι]
+  {σ : Type o} [SetLike σ R] [AddSubmonoidClass σ R] (F : ι → σ) [FilteredRing F]
 
-variable (M : Type*) [AddCommMonoid M] [Module R M] {σM : Type*} [SetLike σM M] [AddSubmonoidClass σM M]
-variable (N : Type*) [AddCommMonoid N] [Module R N] {σN : Type*} [SetLike σN N] [AddSubmonoidClass σN N]
+variable (M : Type*) [AddCommGroup M] [Module R M] {σM : Type*} [SetLike σM M] [AddSubgroupClass σM M]
+variable (N : Type*) [AddCommGroup N] [Module R N] {σN : Type*} [SetLike σN N] [AddSubgroupClass σN N]
 variable (FM : ι → σM) (FN : ι → σN) [FilteredModule F FM] [FilteredModule F FN]
+
+open DirectSum TensorProduct
+
+def indices (i : ι) := {x : ι × ι // x.1 + x.2 = i}
+
+
+-- {(m : M) ⊗ₜ[ℤ] (n : N) | (m : FM i) (n : FN j)}
+instance tmp (i j : ι): AddSubgroup (M ⊗[ℤ] N) where
+  carrier := (FM i) ⊗[ℤ] (FN j)
+  add_mem' := by
+    intro a b ha hb
+  zero_mem' := sorry
+  neg_mem' := sorry
+
+-- instance FilteredPiece (i : ι) : AddSubgroup (M ⊗[ℤ] N) where
+--   carrier := ⨆ (x : (indices i)), (FM x.1.1) ⊗[ℤ] (FN x.1.2)
+
+
+
+-- ⨁ (x : indices i), (FM x.1.1) ⊗[ℤ] (FN x.1.2)
+
+-- instance tensor_filtration : FilteredModule F FilteredPiece where
