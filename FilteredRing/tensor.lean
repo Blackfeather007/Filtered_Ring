312606eed05d2b0d23564505b820e5e89d19e74e
AlbertJ-314 <albertjiao314@gmail.com>
2024-11-17 15:13:40 +0800

    Update tensor.lean

--- FilteredRing/tensor.lean
+++ FilteredRing/tensor.lean
@@ -13,11 +13,9 @@ open DirectSum TensorProduct
 
 def indices (i : ι) := {x : ι × ι // x.1 + x.2 = i}
 
-instance FilteredPiece (i : ι) : Submodule ℤ (M ⊗[ℤ] N) where
-  carrier := ⨆ x : indices i, LinearMap.range (mapIncl (AddSubgroup.toIntSubmodule (AddSubmonoidClass.subtype (FM x.1.1)).range) (AddSubgroup.toIntSubmodule (AddSubmonoidClass.subtype (FN x.1.2)).range))
-  add_mem' := sorry
-  zero_mem' := sorry
-  smul_mem' := sorry
+noncomputable def FilteredPiece (i : ι) : Submodule ℤ (M ⊗[ℤ] N) := ⨆ x : indices i, LinearMap.range
+  (mapIncl (AddSubgroup.toIntSubmodule (AddSubmonoidClass.subtype (FM x.1.1)).range) (AddSubgroup.toIntSubmodule (AddSubmonoidClass.subtype (FN x.1.2)).range))
+
 
 
 
