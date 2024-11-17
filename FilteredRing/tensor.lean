27e61c1b28547d42d75ca8eb2b1bdfa734065e0d
AlbertJ-314 <albertjiao314@gmail.com>
2024-11-17 15:39:41 +0800

    Update tensor.lean

--- FilteredRing/tensor.lean
+++ FilteredRing/tensor.lean
@@ -2,11 +2,13 @@ import Mathlib
 import FilteredRing.Basic
 import FilteredRing.filtered_category
 
+set_option maxHeartbeats 0
+
 variable {R : Type u} {ι : Type v} [CommSemiring R] [OrderedAddCommMonoid ι] [DecidableEq ι]
   {σ : Type o} [SetLike σ R] [AddSubmonoidClass σ R] (F : ι → σ) [FilteredRing F]
 
-variable (M : Type*) [AddCommGroup M] [Module R M] {σM : Type*} [SetLike σM M] [AddSubgroupClass σM M]
-variable (N : Type*) [AddCommGroup N] [Module R N] {σN : Type*} [SetLike σN N] [AddSubgroupClass σN N]
+variable {M : Type*} [AddCommGroup M] [Module R M] {σM : Type*} [SetLike σM M] [AddSubgroupClass σM M]
+variable {N : Type*} [AddCommGroup N] [Module R N] {σN : Type*} [SetLike σN N] [AddSubgroupClass σN N]
 variable (FM : ι → σM) (FN : ι → σN) [FilteredModule F FM] [FilteredModule F FN]
 
 open DirectSum TensorProduct
@@ -14,9 +16,10 @@ open DirectSum TensorProduct
 def indices (i : ι) := {x : ι × ι // x.1 + x.2 = i}
 
 noncomputable def FilteredPiece (i : ι) : Submodule ℤ (M ⊗[ℤ] N) := ⨆ x : indices i, LinearMap.range
-  (mapIncl (AddSubgroup.toIntSubmodule (AddSubmonoidClass.subtype (FM x.1.1)).range) (AddSubgroup.toIntSubmodule (AddSubmonoidClass.subtype (FN x.1.2)).range))
+  (mapIncl (AddSubgroup.toIntSubmodule (AddSubmonoidClass.subtype (FM x.1.1)).range)
+  (AddSubgroup.toIntSubmodule (AddSubmonoidClass.subtype (FN x.1.2)).range))
 
-
-
-
--- instance tensor_filtration : FilteredModule F FilteredPiece where
+instance tensor_filtration : FilteredModule F (FilteredPiece FM FN) where
+  mono := by
+    intro i j ilej
+  smul_mem := sorry
