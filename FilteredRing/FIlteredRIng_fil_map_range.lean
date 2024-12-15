1c16d0fb1984aa68dabbe78f5a478bbfb329e063
yuanyi-350 <kysyy1@126.com>
2024-12-15 11:44:23 +0800

    Update FIlteredRIng_fil_map_range.lean

--- FilteredRing/FIlteredRIng_fil_map_range.lean
+++ FilteredRing/FIlteredRIng_fil_map_range.lean
@@ -2,13 +2,15 @@ import Mathlib
 import FilteredRing.Basic
 import FilteredRing.filtered_category
 
-variable {R : Type*} [Ring R] {ι : Type v} [OrderedCancelAddCommMonoid ι]  [DecidableEq ι]
+section FilteredRing_fil_map_range
+
+variable {R : Type*} [Ring R] {ι : Type v} [OrderedCancelAddCommMonoid ι]
 variable (FR : ι → AddSubmonoid R) [fil : FilteredRing FR]
 variable {S : Type*} [Ring S] (f : R →+* S)
 
-def fil_map  : ι → AddSubmonoid S := fun i ↦ AddSubmonoid.map f (FR i)
+def filring_map  : ι → AddSubmonoid S := fun i ↦ AddSubmonoid.map f (FR i)
 
-instance FilteredRing_fil_map_range (f : R →+* S) : FilteredRing (fil_map FR f) where
+instance FilteredRing_fil_map_range (f : R →+* S) : FilteredRing (filring_map FR f) where
   mono := by
     intro i j ilej y hy
     obtain ⟨x, x_in, x_eq⟩ : ∃ x ∈ FR i , f x = y := hy
@@ -19,40 +21,39 @@ instance FilteredRing_fil_map_range (f : R →+* S) : FilteredRing (fil_map FR f
     simp only [SetLike.mem_coe, FilteredRing.one, map_one, and_self]
   mul_mem := by
     intro i j x y x_in_i y_in_j
-    simp only [fil_map, AddSubmonoid.mem_map] at *
+    simp only [filring_map, AddSubmonoid.mem_map] at *
     obtain ⟨x₁, x_in, x_eq⟩ := x_in_i
     obtain ⟨y₁, y_in, y_eq⟩ := y_in_j
     use x₁ * y₁
     simp only [FilteredRing.mul_mem x_in y_in, map_mul, x_eq, y_eq, and_self]
 
-section FilteredModule_fil_map_map_range
+end FilteredRing_fil_map_range
 
-variable {M : Type w} [AddCommMonoid M] [Module R M]
 
-variable {ιM : Type v} [OrderedAddCommMonoid ιM] [VAdd ι ιM]
-variable {σM : Type*} [SetLike σM M] [AddSubmonoidClass σM M]
-variable {M : Type w} [AddCommMonoid M] [Module R M]
-variable {ιM : Type v} [OrderedAddCommMonoid ιM] [VAdd ι ιM]
-variable {σM : Type*} [SetLike σM M] [AddSubmonoidClass σM M] (f : M →+* N)
 
-def fil_map  : ι → AddSubmonoid S := fun i ↦ AddSubmonoid.map f (FR i)
 
-instance FilteredRing_fil_map_range (f : R →+* S) : FilteredRing (fil_map FR f) where
+section FilteredMod_fil_map_map_range
+
+variable {R : Type u} [CommSemiring R]{ι : Type v} [OrderedCancelAddCommMonoid ι]
+variable (FR : ι → AddSubmonoid R) [fil : FilteredRing FR]
+
+variable {M : Type w} [Semiring M] [Algebra R M] (FM : ι → AddSubmonoid M)
+
+variable {N : Type x} [Semiring N] [Algebra R N] (FN : ι → AddSubmonoid N)
+
+variable [filM : FilteredModule FR FM ] (f : M →+* N)
+
+def filMod_map (α : ι) : AddSubmonoid N := AddSubmonoid.map f (FM α)
+
+instance FilteredMod_fil_map_range (f : M →+* N) : FilteredModule FR (filMod_map FM f) where
   mono := by
     intro i j ilej y hy
     obtain ⟨x, x_in, x_eq⟩ : ∃ x ∈ FR i , f x = y := hy
     use x
     simp only [SetLike.mem_coe, (FilteredRing.mono ilej) x_in, x_eq, and_self]
-  one := by
-    use 1
-    simp only [SetLike.mem_coe, FilteredRing.one, map_one, and_self]
-  mul_mem := by
-    intro i j x y x_in_i y_in_j
-    simp only [fil_map, AddSubmonoid.mem_map] at *
-    obtain ⟨x₁, x_in, x_eq⟩ := x_in_i
-    obtain ⟨y₁, y_in, y_eq⟩ := y_in_j
-    use x₁ * y₁
-    simp only [FilteredRing.mul_mem x_in y_in, map_mul, x_eq, y_eq, and_self]
+  smul_mem := by
+    intro i j x y hx hy
 
+    sorry
 
-end FilteredModule_fil_map_map_range
+end FilteredMod_fil_map_map_range
