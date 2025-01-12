c4cd50b117a5bdbe7b13099d40f130111be57a32
yuanyi-350 <kysyy1@126.com>
2025-01-12 16:15:09 +0800

    Update Grading_to_Filtration-rebuild.lean

--- FilteredRing/Grading_to_Filtration-rebuild.lean
+++ FilteredRing/Grading_to_Filtration-rebuild.lean
@@ -4,7 +4,7 @@ universe u v w
 
 suppress_compilation
 
-section FilRing
+
 variable {R : Type u} [Ring R] {ι : Type v} [OrderedCancelAddCommMonoid ι] [DecidableEq ι]
 
 def F_le (F : ι → AddSubgroup R) : ι → AddSubgroup R := fun i ↦ ⨆ k ≤ i, F k
@@ -32,8 +32,40 @@ instance Graded_to_Filtered (F : ι → AddSubgroup R) [GradedRing F] :
       intro k k_lt_j
       apply h k k_lt_j k (Preorder.le_refl k)
 
-end FilRing
 
+
+instance Graded_to_FilteredRing (F : ι → AddSubgroup R) [GradedRing F] :
+  IsRingFiltration (F_le F) (F_lt F) where
+    __ := Graded_to_Filtered F
+    one_mem :=
+      have : F 0 ≤ ⨆ k, ⨆ (_ : k ≤ 0), F k := (le_biSup F (Preorder.le_refl 0))
+      this SetLike.GradedOne.one_mem
+    mul_mem := by
+      intro i j x y hx hy
+      let S : AddSubgroup R := {
+        carrier := {z | z * y ∈ F_le F (i + j)}
+        add_mem' := fun ha hb ↦ by simp only [Set.mem_setOf_eq, add_mul, add_mem ha.out hb.out]
+        zero_mem' := by simp only [Set.mem_setOf_eq, zero_mul, zero_mem]
+        neg_mem' := by simp only [Set.mem_setOf_eq, neg_mul, neg_mem_iff, imp_self, implies_true]}
+      have : F_le F i ≤ S := by
+        simp only [F_le, iSup_le_iff]
+        intro k hk w hw
+        simp only [AddSubgroup.mem_mk, Set.mem_setOf_eq, S]
+        let T : AddSubgroup R := {
+          carrier := {u | w * u ∈ F_le F (i + j)}
+          add_mem' := fun ha hb ↦ by simp only [Set.mem_setOf_eq, mul_add, add_mem ha.out hb.out]
+          zero_mem' := by simp only [Set.mem_setOf_eq, mul_zero, zero_mem]
+          neg_mem' := by simp only [Set.mem_setOf_eq, mul_neg, neg_mem_iff, imp_self, implies_true]}
+        have : F_le F j ≤ T := by
+          simp only [F_le, iSup_le_iff]
+          intro l hl v hv
+          simp only [AddSubgroup.mem_mk, Set.mem_setOf_eq, T]
+          have : F (k + l) ≤ ⨆ k, ⨆ (_ : k ≤ i + j), F k := by
+            apply le_biSup
+            exact add_le_add hk hl
+          exact this (SetLike.GradedMul.mul_mem hw hv)
+        exact (this hy).out
+      exact this hx
 
 
 
