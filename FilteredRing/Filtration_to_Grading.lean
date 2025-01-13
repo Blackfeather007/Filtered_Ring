e4ee68bbdf35652212979946221c747284e023d2
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2025-01-13 17:40:01 +0800

    port all mul
    still need gnpow

--- FilteredRing/Filtration_to_Grading.lean
+++ FilteredRing/Filtration_to_Grading.lean
@@ -90,8 +90,6 @@ theorem Filtration.mul_equiv_mul [hasGMul F F_lt] ⦃x₁ x₂ : (AddSubgroupCla
   rcases x₂.2 with ⟨x₂', hx₂'⟩
   exact add_mem (hasGMul.F_lt_mul_mem (F := F) hx (by simp [← hy₁'])) (hasGMul.mul_F_lt_mem (F := F) (by simp [← hx₂']) hy)
 
-namespace GradedPiece
-
 variable [hasGMul F F_lt]
 
 def gradedMul {i j : ι} (x : GradedPiece F F_lt i) (y : GradedPiece F F_lt j) : GradedPiece F F_lt (i + j) :=
@@ -100,6 +98,8 @@ def gradedMul {i j : ι} (x : GradedPiece F F_lt i) (y : GradedPiece F F_lt j) :
 instance hMul {i j : ι} :
     HMul (GradedPiece F F_lt i) (GradedPiece F F_lt j) (GradedPiece F F_lt (i + j)) where
   hMul := gradedMul F F_lt
+
+namespace GradedPiece
 
 def mk {i : ι} (x : (AddSubgroupClass.subtype (F i)).range) : GradedPiece F F_lt i := ⟦x⟧
 
@@ -117,6 +117,8 @@ section HEq
 
 lemma GradedPiece.mk_mul {i j : ι} (x : (AddSubgroupClass.subtype (F i)).range) (y : (AddSubgroupClass.subtype (F j)).range) : mk F F_lt x * mk F F_lt y = mk F F_lt (x * y) := rfl
 
+section
+
 omit [OrderedCancelAddCommMonoid ι] [hasGMul F F_lt]
 
 @[simp]
@@ -129,7 +131,7 @@ lemma HEq_rfl {i j : ι} {r : R} (h : i = j)
     HEq (mk F F_lt ⟨r, hi⟩) (mk F F_lt ⟨r, hj⟩) :=
   h ▸ HEq.rfl
 
-theorem fooHEq3 {i j : ι} {x : GradedPiece F F_lt i} {y : GradedPiece F F_lt j} {r s : R}
+theorem HEq_eq_mk_eq {i j : ι} {x : GradedPiece F F_lt i} {y : GradedPiece F F_lt j} {r s : R}
     (h : i = j) (e : r = s) (hi : r ∈ (AddSubgroupClass.subtype (F i)).range) (hj : s ∈ (AddSubgroupClass.subtype (F j)).range)
     (hx : x = mk F F_lt ⟨r, hi⟩) (hy : y = mk F F_lt ⟨s, hj⟩) : HEq x y := by
   rw [hx, hy]
@@ -137,221 +139,129 @@ theorem fooHEq3 {i j : ι} {x : GradedPiece F F_lt i} {y : GradedPiece F F_lt j}
   exact HEq_rfl F F_lt h hi hj
 
 -- Will be easier to use if HMul intances for F i is added and some other refactor is done.
-theorem fooHEq4 {i j : ι} {x : GradedPiece F F_lt i} {y : GradedPiece F F_lt j}
+theorem HEq_eq_mk_coe_eq {i j : ι} {x : GradedPiece F F_lt i} {y : GradedPiece F F_lt j}
     (r : (AddSubgroupClass.subtype (F i)).range) (s : (AddSubgroupClass.subtype (F j)).range)
     (h : i = j) (e : (r : R) = (s : R)) (hx : x = mk F F_lt r) (hy : y = mk F F_lt s) : HEq x y :=
-  fooHEq3 F F_lt h e r.2 (e ▸ s.2) hx hy
+  HEq_eq_mk_eq F F_lt h e r.2 (e ▸ s.2) hx hy
+
+end
+
+lemma gradedMul_def {i j : ι} (x : (AddSubgroupClass.subtype (F i)).range) (y : (AddSubgroupClass.subtype (F j)).range) :
+  mk F F_lt (Filtration.hMul F F_lt i j x y) = gradedMul F F_lt (mk F F_lt x) (mk F F_lt y) := rfl
 
 end HEq
+
+end GradedPiece
+
+namespace GradedMul
+
+open GradedPiece
+
+instance : GradedMonoid.GMul (GradedPiece F F_lt) where
+  mul := gradedMul F F_lt
+
+instance : GradedMonoid.GOne (GradedPiece F F_lt) where
+  one := (1 : GradedPiece F F_lt 0)
+
+variable {F} in
+theorem GradedPiece.mul_zero {i j : ι} (a : GradedPiece F F_lt i) : a * (0 : GradedPiece F F_lt j) = 0 := by
+  rw [← QuotientAddGroup.mk_zero, ← QuotientAddGroup.mk_zero]
+  induction a using Quotient.ind'
+  show Quotient.mk'' _ = Quotient.mk'' _
+  rw [Quotient.eq'']
+  simp only [ZeroMemClass.coe_zero, mul_zero, QuotientAddGroup.leftRel_apply, add_zero, neg_mem_iff]
+  use 0
+  simpa only [AddSubgroupClass.coeSubtype, ZeroMemClass.coe_zero, AddSubgroup.coeSubtype]
+    using (MulZeroClass.mul_zero _).symm
+
+variable {F} in
+theorem GradedPiece.zero_mul {i j : ι} (a : GradedPiece F F_lt i) : (0 : GradedPiece F F_lt j) * a = 0 := by
+  rw [← QuotientAddGroup.mk_zero, ← QuotientAddGroup.mk_zero]
+  induction a using Quotient.ind'
+  change Quotient.mk'' _ = Quotient.mk'' _
+  rw [Quotient.eq'']
+  simp only [ZeroMemClass.coe_zero, zero_mul, QuotientAddGroup.leftRel_apply, add_zero, neg_mem_iff]
+  use 0
+  simpa only [AddSubgroupClass.coeSubtype, ZeroMemClass.coe_zero, AddSubgroup.coeSubtype]
+    using (MulZeroClass.zero_mul _).symm
 
 lemma GradedPiece.HEq_one_mul {i : ι} (x : GradedPiece F F_lt i) : HEq ((1 : GradedPiece F F_lt 0) * x) x := by
   let rx := Quotient.out' x
   let r1 : (AddSubgroupClass.subtype (F 0)).range := ⟨1, AddMonoidHom.mem_range.mpr (by use 1; rfl)⟩
-  apply fooHEq3 F F_lt (zero_add i) (one_mul (rx : R))
-
-  --convert (foo4 F r1 rx).symm
-  --all_goals exact (Quotient.out_eq' x).symm
-
-  repeat sorry
-  /-
-  let rx : F i := Quotient.out' x
-  let r1 : F 0 := ⟨1, FilteredRing.one⟩
-  apply fooHEq3 F (r1 * rx) rx (zero_add i) (one_mul (rx : R))
-  convert (foo4 F r1 rx).symm
-  all_goals exact (Quotient.out_eq' x).symm
-  -/
+  apply HEq_eq_mk_eq F F_lt (zero_add i) (one_mul (rx : R))
+  convert (gradedMul_def F F_lt r1 rx).symm
+  · exact (Quotient.out_eq' x).symm
+  · exact (Quotient.out_eq' x).symm
+  · rcases rx.2 with ⟨rx', hrx'⟩
+    simp [← hrx']
 
 lemma GradedPiece.HEq_mul_one {i : ι} (x : GradedPiece F F_lt i) : HEq (x * (1 : GradedPiece F F_lt 0)) x := by
-  sorry
-  /-
-  let rx : F i := Quotient.out' x
-  let r1 : F 0 := ⟨1, FilteredRing.one⟩
-  apply fooHEq3 F (rx * r1) rx (add_zero i) (mul_one (rx : R))
-  convert (foo4 F rx r1).symm
-  all_goals exact (Quotient.out_eq' x).symm
-  -/
+  let rx := Quotient.out' x
+  let r1 : (AddSubgroupClass.subtype (F 0)).range := ⟨1, AddMonoidHom.mem_range.mpr (by use 1; rfl)⟩
+  apply HEq_eq_mk_eq F F_lt (add_zero i) (mul_one (rx : R))
+  convert (gradedMul_def F F_lt rx r1).symm
+  · exact (Quotient.out_eq' x).symm
+  · exact (Quotient.out_eq' x).symm
+  · rcases rx.2 with ⟨rx', hrx'⟩
+    simp [← hrx']
 
-end GradedPiece
-
-end HasGMul
-
-/-
--- implicit variable?
-variable {F} in
-def GradedPiece.mk {i : ι} (x : F i) : GradedPiece F i := ⟦x⟧
-
-open GradedPiece
-
-instance : OfNat (GradedPiece F i) 0 where
-  ofNat := mk (0 : F i)
-
-@[simp]
-lemma GradedPiece.mk_inj {i : ι} (x y : F i) (h : x = y) : mk x = mk y := congrArg mk h
-
-@[simp]
-lemma GradedPiece.sound {i : ι} (x y : F i) : x ≈ y → mk x = mk y := Quotient.sound
-
-@[simp]
-lemma GradedPiece.exact {i : ι} (x y : F i) : mk x = mk y → x ≈ y := Quotient.exact
-
-/-- to be renamed-/
-@[simp]
-lemma foo1 {i : ι} (x : F i) : mk x = ⟦x⟧ := rfl
-
-/-- to be renamed-/
-@[simp]
-lemma foo2 {i j : ι} (x : F i) (y : GradedPiece F j) : mk x * y = mk (x * y.out') := by
-  induction y using Quotient.ind'
-  rename_i a
-  change Quotient.mk'' _ = Quotient.mk'' _
-  rw [Quotient.eq'']
-  simp only [QuotientAddGroup.leftRel_apply, AddSubgroup.mem_addSubgroupOf]
-  have eq : - (x.1 * a) + x * (Quotient.mk' a).out' = x * (- a + (Quotient.mk' a).out') := by
-    noncomm_ring
-  have ha : -a + (Quotient.mk' a).out' ∈ Filtration.LTSubgroup F j := by
-    rw [← QuotientAddGroup.leftRel_apply, ← Quotient.eq'']
-    exact Eq.symm (Quotient.out_eq' (Quotient.mk' a))
-  convert (Filtration.mul_mem_LTSubgroup' x _ ha )
-  exact SetLike.coe_eq_coe.mp eq
-
-lemma GradedPiece.mk_mul {i j : ι} (x : F i) (y : F j) : mk x * mk y = mk (x * y) := rfl
-
-lemma GradedPiece.mk_zero {i : ι} : mk (0 : F i) = (0 : GradedPiece F i) := rfl
-
--- If refactor with `GradedPiece.mk`, one should change all `⟦_⟧` to `GradedPiece.mk _`
-theorem fooHEq1 {i j : ι} {r : R} (h : i = j) (hi : r ∈ F i) (hj : r ∈ F j) :
-    HEq (⟦⟨r, hi⟩⟧ : GradedPiece F i) (⟦⟨r, hj⟩⟧ : GradedPiece F j) :=
-  h ▸ HEq.rfl
-
-theorem fooHEq3 {i j : ι} {x : GradedPiece F i} {y : GradedPiece F j} (r s : R)
-    (h : i = j) (e : r = s) (hi : r ∈ F i) (hj : s ∈ F j)
-    (hx : x = ⟦⟨r, hi⟩⟧) (hy : y = ⟦⟨s, hj⟩⟧) : HEq x y := by
-  rw [hx, hy]
-  subst e
-  exact fooHEq1 F h hi hj
-
--- Will be easier to use if HMul intances for F i is added and some other refactor is done.
-theorem fooHEq4 {i j : ι} {x : GradedPiece F i} {y : GradedPiece F j} (r : F i) (s : F j)
-    (h : i = j) (e : (r : R) = s) (hx : x = ⟦r⟧) (hy : y = ⟦s⟧) : HEq x y :=
-  fooHEq3 F r s h e r.2 s.2 hx hy
-
-
-@[local simp]
-lemma foo4 {i j : ι} (r : F i) (s : F j) :
-  ⟦(⟨r * s, FilteredRing.mul_mem r.2 s.2⟩ : F (i + j))⟧ = gradedMul F ⟦r⟧ ⟦s⟧ := rfl
-
-/-- Delete-/
--- This is an example of using `fooHEq4` lemma (without refactor)
-lemma foo_bar {i : ι} (x : GradedPiece F i) : HEq (gradedMul F (0 : GradedPiece F 0) x) (0 : GradedPiece F i) := by
-  let rx : F i := Quotient.out' x
-  let r00 : F 0 := ⟨0, zero_mem _⟩
-  let r0i : F i := ⟨0, zero_mem _⟩
-  apply fooHEq3 F (r00 * rx) r0i (zero_add i) (zero_mul (rx : R))
-  convert (foo4 F r00 rx).symm
-  exact (Quotient.out_eq' x).symm
-  rfl
--/
-/-
-instance : GradedMonoid.GMul (GradedPiece F) where
-  mul := gradedMul F
-
-instance : GradedMonoid.GOne (GradedPiece F) where
-  one := .mk ⟨1, FilteredRing.one⟩
-
-variable {F} in
-theorem GradedPiece.mul_zero {i j : ι} (a : GradedPiece F i) : a * (0 : GradedPiece F j) = 0 := by
-  rw [← QuotientAddGroup.mk_zero, ← QuotientAddGroup.mk_zero]
-  induction a using Quotient.ind'
-  change Quotient.mk'' _ = Quotient.mk'' _
-  rw [Quotient.eq'']
-  simp only [ZeroMemClass.coe_zero, mul_zero,
-    QuotientAddGroup.leftRel_apply, add_zero, neg_mem_iff]
-  refine Filtration.mul_mem_LTSubgroup' _ 0 ?h.hy
-  exact zero_mem _
-
-variable {F} in
-theorem GradedPiece.zero_mul {i j : ι} (a : GradedPiece F i) : (0 : GradedPiece F j) * a = 0 := by
-  rw [← QuotientAddGroup.mk_zero, ← QuotientAddGroup.mk_zero]
-  induction a using Quotient.ind'
-  change Quotient.mk'' _ = Quotient.mk'' _
-  rw [Quotient.eq'']
-  simp only [ZeroMemClass.coe_zero, zero_mul,
-    QuotientAddGroup.leftRel_apply, add_zero, neg_mem_iff]
-  refine Filtration.mul_mem_LTSubgroup 0 _ ?h.hy
-  exact zero_mem _
-
-variable {F} in
-theorem GradedPiece.mul_add {i j : ι} (a : GradedPiece F i) (b c : GradedPiece F j) :
+theorem GradedPiece.mul_add {i j : ι} (a : GradedPiece F F_lt i) (b c : GradedPiece F F_lt j) :
     a * (b + c) = a * b + a * c := by
   induction a using Quotient.ind'
   induction b using Quotient.ind'
   induction c using Quotient.ind'
-  change Quotient.mk'' _ = Quotient.mk'' _
+  show Quotient.mk'' _ = Quotient.mk'' _
   rw [Quotient.eq'']
   simp [QuotientAddGroup.leftRel_apply, AddSubgroup.mem_addSubgroupOf]
   rename_i a1 a2 a3
-  have : -(a1 * (a2 + a3)) + (a1 * a2 + a1 * a3) = 0 := by
-    have : -(a1.1 * (a2 + a3)) + (a1 * a2 + a1 * a3) = 0 := by
-      noncomm_ring
-    exact ZeroMemClass.coe_eq_zero.mp this
+  have : -(a1 * (a2 + a3)).1 + ((a1 * a2).1 + (a1 * a3).1) = 0 := by
+    have : -(a1.1 * (a2.1 + a3.1)) + (a1.1 * a2.1 + a1.1 * a3.1) = 0 := by noncomm_ring
+    rw [← this]
+    rfl
   rw [this]
-  exact zero_mem _
+  exact zero_mem (F_lt (i + j))
 
-variable {F} in
-theorem GradedPiece.add_mul {i j : ι} (a b : GradedPiece F i) (c : GradedPiece F j) :
+theorem GradedPiece.add_mul {i j : ι} (a b : GradedPiece F F_lt i) (c : GradedPiece F F_lt j) :
     (a + b) * c = a * c + b * c := by
   induction a using Quotient.ind'
   induction b using Quotient.ind'
   induction c using Quotient.ind'
-  change Quotient.mk'' _ = Quotient.mk'' _
+  show Quotient.mk'' _ = Quotient.mk'' _
   rw [Quotient.eq'']
   simp [QuotientAddGroup.leftRel_apply, AddSubgroup.mem_addSubgroupOf]
   rename_i a1 a2 a3
-  have : -((a1 + a2) * a3) + (a1 * a3 + a2 * a3) = 0 := by
-    have : -((a1.1 + a2) * a3) + (a1 * a3 + a2 * a3) = 0 := by
-      noncomm_ring
-    exact ZeroMemClass.coe_eq_zero.mp this
+  have : -((a1 + a2) * a3).1 + ((a1 * a3).1 + (a2 * a3).1) = 0 := by
+    have : -((a1.1 + a2.1) * a3.1) + (a1.1 * a3.1 + a2.1 * a3.1) = 0 := by noncomm_ring
+    rw [← this]
+    rfl
   rw [this]
-  exact zero_mem _
+  exact zero_mem (F_lt (i + j))
 
-
-variable {F} in
-lemma GradedPiece.HEq_one_mul {i : ι} (x : GradedPiece F i) : HEq ((1 : GradedPiece F 0) * x) x := by
-  let rx : F i := Quotient.out' x
-  let r1 : F 0 := ⟨1, FilteredRing.one⟩
-  apply fooHEq3 F (r1 * rx) rx (zero_add i) (one_mul (rx : R))
-  convert (foo4 F r1 rx).symm
-  all_goals exact (Quotient.out_eq' x).symm
-
-variable {F} in
-lemma GradedPiece.HEq_mul_one {i : ι} (x : GradedPiece F i) : HEq (x * (1 : GradedPiece F 0)) x := by
-  let rx : F i := Quotient.out' x
-  let r1 : F 0 := ⟨1, FilteredRing.one⟩
-  apply fooHEq3 F (rx * r1) rx (add_zero i) (mul_one (rx : R))
-  convert (foo4 F rx r1).symm
-  all_goals exact (Quotient.out_eq' x).symm
-
-variable {F} in
 lemma GradedPiece.HEq_mul_assoc {i j k : ι}
-    (a : GradedPiece F i) (b : GradedPiece F j) (c : GradedPiece F k) :
+    (a : GradedPiece F F_lt i) (b : GradedPiece F F_lt j) (c : GradedPiece F F_lt k) :
     HEq (a * b * c) (a * (b * c)) := by
-  let ra : F i := Quotient.out' a
-  let rb : F j := Quotient.out' b
-  let rc : F k := Quotient.out' c
-  apply fooHEq3 F (ra * rb * rc) (ra * (rb * rc)) (add_assoc i j k) (mul_assoc (ra : R) rb rc)
+  let ra := Quotient.out' a
+  let rb := Quotient.out' b
+  let rc := Quotient.out' c
+  apply HEq_eq_mk_eq F F_lt (add_assoc i j k) (mul_assoc (ra : R) rb rc)
   · show a * b * c = ⟦ra * rb * rc⟧
-    convert (foo4 F (ra * rb) rc).symm
-    · convert (foo4 F ra rb).symm
+    convert (gradedMul_def F F_lt (ra * rb) rc).symm
+    · convert (gradedMul_def F F_lt ra rb).symm
       · exact (Quotient.out_eq' a).symm
       · exact (Quotient.out_eq' b).symm
     · exact (Quotient.out_eq' c).symm
   · show a * (b * c) = ⟦ra * (rb * rc)⟧
-    convert (foo4 F ra (rb * rc)).symm
+    convert (gradedMul_def F F_lt ra (rb * rc)).symm
     · exact (Quotient.out_eq' a).symm
-    · convert (foo4 F rb rc).symm
+    · convert (gradedMul_def F F_lt rb rc).symm
       · exact (Quotient.out_eq' b).symm
       · exact (Quotient.out_eq' c).symm
 
+end GradedMul
+
+end HasGMul
+
+/-
 variable {F} in
 lemma Filtration.pow_mem (n : ℕ) {i : ι} (x : F i) : (x : R) ^ n ∈ F (n • i) := by
   induction' n with d hd
@@ -369,6 +279,21 @@ lemma GradedPiece.gnpow_zero' {i : ι} (x : GradedPiece F i) :
   let r1 : F 0 := ⟨1, FilteredRing.one⟩
   have smul_eq : 0 • i = 0 := by simp only [zero_smul] --?
   apply fooHEq3 F (rx ^ 0) r1 (smul_eq) (pow_zero rx.1) (Filtration.pow_mem 0 rx) r1.2 rfl rfl
+
+@[simp]
+lemma foo2 {i j : ι} (x : F i) (y : GradedPiece F j) : mk x * y = mk (x * y.out') := by
+  induction y using Quotient.ind'
+  rename_i a
+  change Quotient.mk'' _ = Quotient.mk'' _
+  rw [Quotient.eq'']
+  simp only [QuotientAddGroup.leftRel_apply, AddSubgroup.mem_addSubgroupOf]
+  have eq : - (x.1 * a) + x * (Quotient.mk' a).out' = x * (- a + (Quotient.mk' a).out') := by
+    noncomm_ring
+  have ha : -a + (Quotient.mk' a).out' ∈ Filtration.LTSubgroup F j := by
+    rw [← QuotientAddGroup.leftRel_apply, ← Quotient.eq'']
+    exact Eq.symm (Quotient.out_eq' (Quotient.mk' a))
+  convert (Filtration.mul_mem_LTSubgroup' x _ ha )
+  exact SetLike.coe_eq_coe.mp eq
 
 variable {F} in
 lemma GradedPiece.gnpow_succ' (n : ℕ) {i : ι} (x : GradedPiece F i) :
