9ddf1fa3cd189e2d9708d84e057bcfa68c22f64d
yuanyi-350 <kysyy1@126.com>
2025-01-13 19:27:52 +0800

    change naem

--- FilteredRing/RingHom induce FilterHom.lean
+++ FilteredRing/RingHom induce FilterHom.lean
@@ -20,26 +20,39 @@ class SubgroupClasscomap (f : R →+* S) where
   comap (y : σS) : σR
   property (y : σS) : (comap y : Set R) = ⇑f ⁻¹' y
 
-variable (FR : ι → σR) (FR_lt :  outParam <| ι → σR) (f : R →+* S) [fil : IsRingFiltration FR FR_lt]
-[SubgroupClassHom σR σS f]
+variable (FR : ι → σR) (FR_lt : outParam <| ι → σR) (f : R →+* S)
+  [SubgroupClassHom σR σS f]
 
 open SubgroupClassHom Set
-instance Filtered_fil_map_range [SubgroupClasscomap σR σS f]
- : IsFiltration (FS σR σS FR f) (FS_lt σR σS FR_lt f) where
+
+
+private lemma ele_map_to_image [SubgroupClasscomap σR σS f] {A: σR}{x : S} :
+    x ∈ ⇑f '' (A : Set R) → x ∈ (map f <| A : σS):= by
+  show x ∈ ⇑f '' (A : Set R) → x ∈ (((map f <| A) : σS) : Set S)
+  simp only[← image_coe_eq_coe_map <| A, imp_self]
+
+private lemma map_to_image [SubgroupClasscomap σR σS f] {A B: σR} :
+    ⇑f '' (A : Set R) ≤ ⇑f '' (B : Set R) → (map f <| A : σS) ≤ (map f <| B : σS):= by
+  show ⇑f '' (A : Set R) ≤ ⇑f '' (B : Set R) → (((map f <| A) : σS) : Set S) ≤ (((map f <| B) : σS) : Set S)
+  simp only [image_subset_iff, ← image_coe_eq_coe_map <| A, ← image_coe_eq_coe_map <| B, imp_self]
+
+
+
+instance Filtered_fil_map_range [fil : IsFiltration FR FR_lt]
+[SubgroupClasscomap σR σS f] : IsFiltration (FS σR σS FR f) (FS_lt σR σS FR_lt f) where
   mono := by
     intro i j i_le_j
-    show (((map f <| FR i) : σS) : Set S) ≤ (((map f <| FR j) : σS) : Set S)
-    rw[← image_coe_eq_coe_map <| FR i, ← image_coe_eq_coe_map <| FR j]
+    apply map_to_image
     exact le_iff_subset.mpr <| image_mono <| IsFiltration.mono i_le_j
   is_le := by
     intro j i i_lt_j
-    show (((map f <| FR i) : σS) : Set S) ≤ (((map f <| FR_lt j) : σS) : Set S)
-    rw[← image_coe_eq_coe_map <| FR i, ← image_coe_eq_coe_map <| FR_lt j]
+    apply map_to_image
     exact le_iff_subset.mpr <| image_mono <| IsFiltration.is_le i_lt_j
   is_sup := by
     intro B j h
     show ((map f (FR_lt j) : σS): Set S) ≤ (B : Set S)
     rw[← image_coe_eq_coe_map <| FR_lt j]
+
     refine le_iff_subset.mpr <| image_subset_iff.mpr ?_
 
     have h : ∀ i < j, ↑(FR i) ≤ ⇑f ⁻¹' ↑B := by
@@ -54,25 +67,34 @@ instance Filtered_fil_map_range [SubgroupClasscomap σR σS f]
     rw[← this] at h ⊢
     exact IsFiltration.is_sup (SubgroupClasscomap.comap f B : σR) j h
 
--- instance [SubgroupClasscomap σR σS f] : IsRingFiltration (FS σR σS FR f) (FS_lt σR σS FR_lt f) := sorry
+instance [fil : IsRingFiltration FR FR_lt] [SubgroupClasscomap σR σS f] :
+  IsRingFiltration (FS σR σS FR f) (FS_lt σR σS FR_lt f) where
+    __ := Filtered_fil_map_range σR σS FR FR_lt f
+    one_mem := by
+      apply ele_map_to_image
+      use 1
+      simp only [SetLike.mem_coe, IsRingFiltration.one_mem, map_one, and_self]
+    mul_mem := by
+      intro i j x y x_in_i y_in_j
 
--- where
---   mono :=
---   one := by
---     use 1
---     simp only [SetLike.mem_coe, FilteredRing.one, map_one, and_self]
---   mul_mem := by
---     intro i j x y x_in_i y_in_j
---     simp only [filring_map, AddSubgroup.mem_map] at *
---     obtain ⟨x₁, x_in, x_eq⟩ := x_in_i
---     obtain ⟨y₁, y_in, y_eq⟩ := y_in_j
---     use x₁ * y₁
---     simp only [FilteredRing.mul_mem x_in y_in, map_mul, x_eq, y_eq, and_self]
+      apply ele_map_to_image
+
+      have x_in_i : x ∈ ((map f (FR i) : σS) : Set S) := x_in_i
+      rw[← image_coe_eq_coe_map <| FR i] at x_in_i
+
+      have y_in_j : y ∈ ((map f (FR j) : σS) : Set S) := y_in_j
+      rw[← image_coe_eq_coe_map <| FR j] at y_in_j
+
+      obtain ⟨x₁, x_in, x_eq⟩ := x_in_i
+      obtain ⟨y₁, y_in, y_eq⟩ := y_in_j
+      use x₁ * y₁
+      simp only [SetLike.mem_coe, IsRingFiltration.mul_mem x_in y_in, map_mul,
+        Mathlib.Tactic.LinearCombination'.mul_pf x_eq y_eq, and_self]
 
 
 end FilteredRing_fil_map_range
 
-/--/
+/-
 
 
 section FilteredMod_fil_map_map_range
@@ -138,4 +160,4 @@ instance FilAlg_map_range (f : A →ₐ[R] B) : FilteredAlgebra (filAlg_map 𝒜
     use x₁ * y₁
     simp only [SetLike.mem_coe, FilteredRing.mul_mem x_in y_in, map_mul, x_eq, y_eq, and_self]
 
-end FilteredMod_fil_map_map_range
+end FilteredMod_fil_map_map_range-/
