08d66dd0e80f9ab5cac36d019e83978acf8792c0
yuanyi-350 <kysyy1@126.com>
2025-01-13 22:26:18 +0800

    Update RingHom induce FilterHom.lean

--- FilteredRing/RingHom induce FilterHom.lean
+++ FilteredRing/RingHom induce FilterHom.lean
@@ -66,18 +66,14 @@ variable {R : Type*} [Ring R] (σR : Type*) [SetLike σR R] [AddSubgroupClass σ
 {S : Type*} [Ring S] (σS : Type*) [SetLike σS S] [AddSubgroupClass σS S]
 
 
-def FS (FR : ι → σR)(f : R →+* S)[SubmonoidClassHom σR σS f] : ι → σS :=
-  fun i ↦ SubmonoidClassHom.map f (FR i)
+def FS (FR : ι → σR)(f : R →+* S)[SubmonoidClassHom σR σS f][SubmonoidClassHom σR σS f] :
+ ι → σS := FB σR σS FR f
 
-def FS_lt (FR_lt : ι → σR) (f : R →+* S) [SubmonoidClassHom σR σS f] :  outParam <| ι → σS :=
-  fun i ↦ SubmonoidClassHom.map f (FR_lt i)
+def FS_lt (FR_lt : ι → σR) (f : R →+* S) [SubmonoidClassHom σR σS f] [SubmonoidClassHom σR σS f]:
+ outParam <| ι → σS := FB_lt σR σS FR_lt f
 
 variable (FR : ι → σR) (FR_lt :  outParam <| ι → σR) (f : R →+* S) [fil : IsRingFiltration FR FR_lt]
 [SubmonoidClassHom σR σS f]
-
-
-
-#check HomtoFiltration
 
 open SubmonoidClassHom Set
 
@@ -116,126 +112,6 @@ end RingHomtoFiltration
 
 
 
-/-
-
-section RingHomtoFiltration
-
-variable {R : Type*} [Ring R] (σR : Type*) [SetLike σR R] [AddSubgroupClass σR R]
-{S : Type*} [Ring S] (σS : Type*) [SetLike σS S] [AddSubgroupClass σS S]
-
-class SubgroupClassHom (f : R →+* S) where
-  map : σR → σS
-  image_coe_eq_coe_map (x : σR) : f '' (x : Set R) = map x
-
-def FS (FR : ι → σR)(f : R →+* S)[SubgroupClassHom σR σS f] : ι → σS :=
-  fun i ↦ SubgroupClassHom.map f (FR i)
-
-def FS_lt (FR_lt : ι → σR) (f : R →+* S) [SubgroupClassHom σR σS f] :  outParam <| ι → σS :=
-  fun i ↦ SubgroupClassHom.map f (FR_lt i)
-
-class SubgroupClasscomap (f : R →+* S) where
-  comap (y : σS) : σR
-  property (y : σS) : (comap y : Set R) = ⇑f ⁻¹' y
-
-variable (FR : ι → σR) (FR_lt :  outParam <| ι → σR) (f : R →+* S) [fil : IsRingFiltration FR FR_lt]
-[SubgroupClassHom σR σS f]
-
-
-
-
-open SubgroupClassHom Set
-instance Filtered_fil_map_range [SubgroupClasscomap σR σS f]
- : IsFiltration (FS σR σS FR f) (FS_lt σR σS FR_lt f) where
-  mono := by
-    intro i j i_le_j
-    show (((map f <| FR i) : σS) : Set S) ≤ (((map f <| FR j) : σS) : Set S)
-    rw[← image_coe_eq_coe_map <| FR i, ← image_coe_eq_coe_map <| FR j]
-    exact le_iff_subset.mpr <| image_mono <| IsFiltration.mono i_le_j
-  is_le := by
-    intro j i i_lt_j
-    show (((map f <| FR i) : σS) : Set S) ≤ (((map f <| FR_lt j) : σS) : Set S)
-    rw[← image_coe_eq_coe_map <| FR i, ← image_coe_eq_coe_map <| FR_lt j]
-    exact le_iff_subset.mpr <| image_mono <| IsFiltration.is_le i_lt_j
-  is_sup := by
-    intro B j h
-    show ((map f (FR_lt j) : σS): Set S) ≤ (B : Set S)
-    rw[← image_coe_eq_coe_map <| FR_lt j]
-    refine le_iff_subset.mpr <| image_subset_iff.mpr ?_
-
-    have h : ∀ i < j, ↑(FR i) ≤ ⇑f ⁻¹' ↑B := by
-      intro i i_lt_j
-      have : (⇑f '' (FR i) : Set S) ≤ B := by
-        have : ((map f (FR i) : σS) : Set S) ≤ (B : Set S) := h i i_lt_j
-        rw[← image_coe_eq_coe_map <| FR i] at this
-        exact this
-      exact le_iff_subset.mpr <| image_subset_iff.mp this
-
-    have : (SubgroupClasscomap.comap f B : σR) = ⇑f ⁻¹' B := SubgroupClasscomap.property B
-    rw[← this] at h ⊢
-    exact IsFiltration.is_sup (SubgroupClasscomap.comap f B : σR) j h
-
-
-private lemma ele_map_to_image [SubgroupClasscomap σR σS f] {A: σR}{x : S} :
-    x ∈ ⇑f '' (A : Set R) → x ∈ (map f <| A : σS):= by
-  show x ∈ ⇑f '' (A : Set R) → x ∈ (((map f <| A) : σS) : Set S)
-  simp only[← image_coe_eq_coe_map <| A, imp_self]
-
-private lemma map_to_image [SubgroupClasscomap σR σS f] {A B: σR} :
-    ⇑f '' (A : Set R) ≤ ⇑f '' (B : Set R) → (map f <| A : σS) ≤ (map f <| B : σS):= by
-  show ⇑f '' (A : Set R) ≤ ⇑f '' (B : Set R) → (((map f <| A) : σS) : Set S) ≤ (((map f <| B) : σS) : Set S)
-  simp only [image_subset_iff, ← image_coe_eq_coe_map <| A, ← image_coe_eq_coe_map <| B, imp_self]
-
-instance [fil : IsRingFiltration FR FR_lt] [SubgroupClasscomap σR σS f] :
-  IsRingFiltration (FS σR σS FR f) (FS_lt σR σS FR_lt f) where
-    __ := Filtered_fil_map_range σR σS FR FR_lt f
-    one_mem := by
-      apply ele_map_to_image
-      use 1
-      simp only [SetLike.mem_coe, IsRingFiltration.one_mem, map_one, and_self]
-    mul_mem := by
-      intro i j x y x_in_i y_in_j
-
-      apply ele_map_to_image
-
-      have x_in_i : x ∈ ((map f (FR i) : σS) : Set S) := x_in_i
-      rw[← image_coe_eq_coe_map <| FR i] at x_in_i
-
-      have y_in_j : y ∈ ((map f (FR j) : σS) : Set S) := y_in_j
-      rw[← image_coe_eq_coe_map <| FR j] at y_in_j
-
-      obtain ⟨x₁, x_in, x_eq⟩ := x_in_i
-      obtain ⟨y₁, y_in, y_eq⟩ := y_in_j
-      use x₁ * y₁
-      simp only [SetLike.mem_coe, IsRingFiltration.mul_mem x_in y_in, map_mul,
-        Mathlib.Tactic.LinearCombination'.mul_pf x_eq y_eq, and_self]
-
-
-
-
-end RingHomtoFiltration
-
-
-
-section RingHom_to_FilterHom
-
-
-variable (FR : ι → σR) (FR_lt : outParam <| ι → σR) (f : R →+* S)
-  [SubgroupClassHom σR σS f]
-
-open
-
-
-
-
-
-
-
-
-
-
-end RingHom_to_FilterHom
-
-
 section
 
 variable {R : Type*} [Ring R] (σR : Type*) [SetLike σR R] [AddSubgroupClass σR R]
@@ -254,30 +130,29 @@ class SubmoduleClassHom (f : M →+ N) where
   image_coe_eq_coe_map (x : σM) : f '' (x : Set M) = map x
 
 #check SubmoduleClassHom σM σN f
-def FN (FM : ι → σM) (f : M →+ N)[SubmoduleClassHom σM σN f] : ι → σN :=
-  fun i ↦ SubmoduleClassHom.map f (FM i)
 
-def FN_lt (FM_lt : ι → σM) (f : M →+ N) [SubmoduleClassHom σM σN f] :  outParam <| ι → σN :=
-  fun i ↦ SubmoduleClassHom.map f (FM_lt i)
+def FN (FM : ι → σM) (f : M →+ N)[SubmoduleClassHom σM σN f] [SubmonoidClassHom σM σN f]
+: ι → σN := FB σM σN FM f
 
-#check IsModuleFiltration
-variable [ SubmoduleClassHom σM σN f]
--- #check IsModuleFiltration FR FR_lt (FN σM σN FM f) (FN_lt σM σN FM_lt f)
+def FN_lt (FM_lt : ι → σM) (f : M →+ N) [SubmoduleClassHom σM σN f] [SubmonoidClassHom σM σN f]
+: outParam <| ι → σN := FB_lt σM σN FM_lt f
 
-#check Filtered_fil_map_range
+variable [SubmoduleClassHom σM σN f]
+
+#check HomtoFiltration
+-- FB = FN
 
 lemma aaa : IsFiltration (FN σM σN FM f) (FN_lt σM σN FM_lt f) := by
-  apply Filtered_fil_map_range
+  apply HomtoFiltration
+  sorry
+
+#check aaa σM FM FM_lt σN f
 
 theorem FilMod_map_range   :
  IsModuleFiltration FR FR_lt (FN σM σN FM f) (FN_lt σM σN FM_lt f) where
-
-  __ :=
-
-    -- exact ι
-
-    sorry
-  smul_mem := sorry
+   __ := aaa σM FM FM_lt σN f
+   smul_mem := sorry
+  -- smul_mem := sorry
 --   mono := by
 --
 --   smul_mem := by
@@ -327,4 +202,3 @@ instance FilAlg_map_range (f : A →ₐ[R] B) : FilteredAlgebra (filAlg_map 𝒜
     simp only [SetLike.mem_coe, FilteredRing.mul_mem x_in y_in, map_mul, x_eq, y_eq, and_self]
 
 end FilteredMod_fil_map_map_range-/
--/
