31a21b088a81c8b4a989a14f2508b7d01e8588bb
yuanyi-350 <kysyy1@126.com>
2025-01-22 07:45:53 +0800

    change condition to GaliosConnection

--- FilteredRing/InduceMaps.lean
+++ FilteredRing/InduceMaps.lean
@@ -11,7 +11,7 @@ and extend it to get the filtration on ring and module.
 
 # Main definitions
 
-* `SubmonoidClassHom`
+* `SetLikeHom`
 
 * `HomtoFiltration`
 
@@ -30,23 +30,23 @@ variable {A : Type*} (σA : Type*) [SetLike σA A] {B : Type*}  (σB : Type*) [S
 We need these necessary conditions to describe σA and σB,
  thus we can ensure f can induce a map from σA → σB
 -/
-class SubmonoidClassHom (f : A → B) where
+class SetLikeHom (f : A → B) where
   map : σA → σB
   comap : σB → σA
   coe_map (x : σA) : f '' (x : Set A) = map x
-  coe_comap (y : σB) : (comap y : Set A) = f ⁻¹' y
+  galois : GaloisConnection map comap
 
-def FB (FA : ι → σA) (f : A → B) [SubmonoidClassHom σA σB f]: ι → σB :=
-  fun i ↦ SubmonoidClassHom.map f (FA i)
+open SetLikeHom Set
+def FB (FA : ι → σA) (f : A → B) [SetLikeHom σA σB f]: ι → σB :=
+  fun i ↦ map f (FA i)
 
-def FB_lt (FA_lt : ι → σA) (f : A → B) [SubmonoidClassHom σA σB f] : outParam <| ι → σB :=
-  fun i ↦ SubmonoidClassHom.map f (FA_lt i)
+def FB_lt (FA_lt : ι → σA) (f : A → B) [SetLikeHom σA σB f] : outParam <| ι → σB :=
+  fun i ↦ map f (FA_lt i)
 
-open SubmonoidClassHom Set
 /-
 When FA and FA_lt is a filtration of A, then f: A → B induce a filtration of B
 -/
-instance HomtoFiltration [fil : IsFiltration FA FA_lt] [SubmonoidClassHom σA σB f] :
+instance HomtoFiltration [fil : IsFiltration FA FA_lt] [SetLikeHom σA σB f] :
  IsFiltration (FB σA σB FA f) (FB_lt σA σB FA_lt f) (ι := ι) where
   mono {i j i_le_j}:= by
     show (((map f <| FA i) : σB) : Set B) ≤ (((map f <| FA j) : σB) : Set B)
@@ -57,18 +57,12 @@ instance HomtoFiltration [fil : IsFiltration FA FA_lt] [SubmonoidClassHom σA σ
     rw[← coe_map <| FA i, ← coe_map <| FA_lt j]
     exact le_iff_subset.mpr <| image_mono <| IsFiltration.is_le i_lt_j
   is_sup {Sup j h}:= by
-    show ((map f (FA_lt j) : σB): Set B) ≤ (Sup : Set B)
-    rw[← coe_map <| FA_lt j]
-    refine le_iff_subset.mpr <| image_subset_iff.mpr ?_
-    have h : ∀ i < j, ↑(FA i) ≤ f ⁻¹' ↑Sup := by
+    apply galois.l_le
+    have h : ∀ i < j, FA i ≤ comap f Sup := by
       intro i i_lt_j
-      have : f '' (FA i) ≤ Sup := by
-        have : ((map f (FA i) : σB) : Set B) ≤ (Sup : Set B) := h i i_lt_j
-        rwa[← coe_map <| FA i] at this
-      exact le_iff_subset.mpr <| image_subset_iff.mp this
-    have : (comap f Sup : σA) = f ⁻¹' Sup := coe_comap Sup
-    rw[← this] at h ⊢
-    exact IsFiltration.is_sup (comap f Sup : σA) j h
+      apply galois.le_u
+      exact h i i_lt_j
+    exact IsFiltration.is_sup (comap f Sup) j h
 
 end HomtoFiltration
 
@@ -80,16 +74,16 @@ section RingHomtoFiltration
 variable {R : Type*} [Ring R] (σR : Type*) [SetLike σR R] [AddSubgroupClass σR R]
 {S : Type*} [Ring S] (σS : Type*) [SetLike σS S] [AddSubgroupClass σS S]
 
-def FS (FR : ι → σR)(f : R →+* S)[SubmonoidClassHom σR σS f][SubmonoidClassHom σR σS f] :
+def FS (FR : ι → σR)(f : R →+* S)[SetLikeHom σR σS f][SetLikeHom σR σS f] :
  ι → σS := FB σR σS FR f
 
-def FS_lt (FR_lt : ι → σR) (f : R →+* S) [SubmonoidClassHom σR σS f]
-[SubmonoidClassHom σR σS f] : outParam <| ι → σS := FB_lt σR σS FR_lt f
+def FS_lt (FR_lt : ι → σR) (f : R →+* S) [SetLikeHom σR σS f]
+[SetLikeHom σR σS f] : outParam <| ι → σS := FB_lt σR σS FR_lt f
 
 variable (FR : ι → σR) (FR_lt : outParam <| ι → σR) (f : R →+* S)
-[IsRingFiltration FR FR_lt] [SubmonoidClassHom σR σS f]
+[IsRingFiltration FR FR_lt] [SetLikeHom σR σS f]
 
-open SubmonoidClassHom Set
+open SetLikeHom Set
 instance ele_map_to_image {A: σR}{x : S} : x ∈ f '' (A : Set R) → x ∈ (map f <| A : σS):= by
   show x ∈ f '' (A : Set R) → x ∈ (((map f <| A) : σS) : Set S)
   simp only[← coe_map <| A, imp_self]
@@ -138,17 +132,17 @@ variable {N : Type*} [AddCommMonoid N] [Module R N] (σN : Type*) [SetLike σN N
 
 variable [filM : IsModuleFiltration FR FR_lt FM FM_lt] (f : M →ₗ[R] N)
 
-def FN (FM : ι → σM) (f : M →ₗ[R] N)[SubmonoidClassHom σM σN f] [SubmonoidClassHom σM σN f]
+def FN (FM : ι → σM) (f : M →ₗ[R] N)[SetLikeHom σM σN f] [SetLikeHom σM σN f]
 : ι → σN := FB σM σN FM f
 
-def FN_lt (FM_lt : ι → σM) (f : M →ₗ[R] N) [SubmonoidClassHom σM σN f]
-[SubmonoidClassHom σM σN f] : outParam <| ι → σN := FB_lt σM σN FM_lt f
+def FN_lt (FM_lt : ι → σM) (f : M →ₗ[R] N) [SetLikeHom σM σN f]
+[SetLikeHom σM σN f] : outParam <| ι → σN := FB_lt σM σN FM_lt f
 
 /-
 When FM and FM_lt is a filtration of M, then module hom f: M → N induce a filtration of B
 -/
-open SubmonoidClassHom
-instance ModuleHomtoFiltration [SubmonoidClassHom σM σN f] :
+open SetLikeHom
+instance ModuleHomtoFiltration [SetLikeHom σM σN f] :
     IsModuleFiltration FR FR_lt (FN σM σN FM f) (FN_lt σM σN FM_lt f) where
   __ := HomtoFiltration σM σN (f := f.toFun) (ι := ι) (FA := FM) (FA_lt := FM_lt)
   smul_mem {i j r n hr hn}:= by
@@ -165,21 +159,3 @@ instance ModuleHomtoFiltration [SubmonoidClassHom σM σN f] :
     simp only [SetLike.mem_coe, this, map_smul, and_self]
 
 end ModuleHomtoFiltration
-
-
-
-/-
-section AlgebraHomtoFiltration
-
-variable {R : Type*} [CommSemiring R]
-{𝒜 : Type*}[Ring 𝒜][Algebra R 𝒜]{σA : Type*}[SetLike σA 𝒜][AddSubmonoidClass σA 𝒜]
-[SMulMemClass σA R 𝒜] (FA : ι → σA)(FA_lt : outParam <| ι → σA)
-{ℬ : Type*}[Ring ℬ][Algebra R ℬ]{σB : Type*}[SetLike σB ℬ][AddSubmonoidClass σB ℬ]
-[SMulMemClass σB R ℬ](f : 𝒜 →ₐ[R] ℬ)
-
-instance AlgebraHomtoFiltration [SubmonoidClassHom σA σB f] [IsAlgebraFiltration FA FA_lt]:
-    IsAlgebraFiltration (FB σA σB FA f.toFun) (FB_lt σA σB FA_lt f.toFun) where
-  __ := RingHomtoFiltration σA σB FA FA_lt f.toRingHom
-
-end AlgebraHomtoFiltration
--/
